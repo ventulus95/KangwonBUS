4bffabd20e4131d6063d35ac5ed4284106a5ed85
이창섭 <ventulus95@gmail.com>
2020-02-13 15:47:55 +0900

    Getmapping API 서버 구현 - XML 파싱을 위한 DTO -> 하나안에 두개의 XML 처리 가능하게 만듬. - API를 통한 전달 Get전달을 쉽게 하기위한 RestapiController - 버스정류장에 몇분 남았는지? 그리고 몇정거장 남았는지 확인해주는 서비스로직 - URI를 한글로 쳐도 그에 맞게 Nodeid를 검색해줄 수 있는 hashmap과 restTemplate 빈 생성 configuration

--- src/main/java/com/jistol/boilerplate/DTO/Response.java
+++ src/main/java/com/jistol/boilerplate/DTO/Response.java
@@ -12,7 +12,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @ToString
 @XmlRootElement(name ="response")
 public class Response {
-//    private Header header;
+    private Header header;
     private Body body;
 
 }
